ca1bf25d5eeb058302089726f900ac7ac6579ec6
sakachris <sakachris90@mail.com>
2024-03-13 17:39:59 +0300

    mongodb

---
+++ utils/db.js
@@ -0,0 +1,33 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+    constructor() {
+        const host = process.env.DB_HOST || 'localhost';
+        const port = process.env.DB_PORT || 27017;
+        const database = process.env.DB_DATABASE || 'files_manager';
+        const url = `mongodb://${host}:${port}/${database}`;
+        this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+        this.client.connect();
+        this.db = this.client.db(database);
+    }
+
+    async isAlive() {
+        try {
+            await this.client.db().admin().ping();
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+
+    async nbUsers() {
+        return this.db.collection('users').estimatedDocumentCount();
+    }
+
+    async nbFiles() {
+        return this.db.collection('files').estimatedDocumentCount();
+    }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
